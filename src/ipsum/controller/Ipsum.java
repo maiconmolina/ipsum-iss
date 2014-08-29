0ca19da54f6ca88feb3c2d6d41e0948d3d033530
Luis <Luis@Luis-Fernando>
2014-08-29 19:37:13 -0300

    novo

--- src/ipsum/controller/Ipsum.java
+++ src/ipsum/controller/Ipsum.java
@@ -1,12 +1,13 @@
 package ipsum.controller;
 
+import fornecedor.model.Fornecedor;
+import fornecedor.model.FornecedorDAOImpl;
 import ipsum.view.TelaStart;
 
 
 public class Ipsum {
 
     public static void main(String[] args) {
-        
         TelaStart tela = new TelaStart();
         tela.setVisible(true);
     }
